9631b108761095f2ed202725d70e058abbca9d45
duhnunes <duhnunes.dev@gmail.com>
2024-09-23 08:36:07 -0300

    Change: Little detail in button to can't select text and not drag image

--- src/app/(auth)/login/components/button.tsx
+++ src/app/(auth)/login/components/button.tsx
@@ -13,11 +13,17 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         type="button"
-        className="bg-gray-600 text-gray-200 py-5 px-6 rounded-md inline-flex items-center gap-5 hover:brightness-90"
+        className="bg-gray-600 text-gray-200 py-5 px-6 rounded-md inline-flex items-center gap-5 hover:brightness-90 select-none"
         {...props}
         ref={ref}
       >
-        <Image src={src} width={32} height={32} alt="" />
+        <Image
+          src={src}
+          width={32}
+          height={32}
+          alt=""
+          className="pointer-events-none"
+        />
         <span className="text-gray-200 text-lg font-bold">{children}</span>
       </button>
     )

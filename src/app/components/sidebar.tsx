9a0ca47df005ea2c61c8ecfa3a6d185c978cc612
duhnunes <duhnunes.dev@gmail.com>
2024-09-24 10:41:09 -0300

    Add: Menu Sidebar

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import logo from '../../../public/logo.svg'
+import { Button } from './ui/button'
+import { BinocularsIcon, ChartLine, LogIn } from 'lucide-react'
+
+export const Sidebar = () => {
+  return (
+    <aside className="fixed top-5 left-5 bottom-5 rounded-xl bg-gray-700 pt-10 pb-6 px-12 overflow-hidden">
+      <section className="flex flex-col h-full">
+        <Image
+          src={logo}
+          width={128}
+          height={32}
+          className="mb-16 pointer-events-none"
+          alt=""
+        />
+        <nav className="flex-1">
+          <ul className="flex flex-col gap-4 text-gray-400 relative">
+            <li className="inline-flex items-center text-gray-100">
+              <div className="bg-gradient-to-b from-from to-to w-1 h-6 -left-4 absolute rounded-full" />
+              <Button variant="link" size="link">
+                <ChartLine className="size-5" />
+                Início
+              </Button>
+            </li>
+            <li className="inline-flex items-center">
+              <Button variant="link" size="link">
+                <BinocularsIcon className="size-5" />
+                Explorar
+              </Button>
+            </li>
+          </ul>
+        </nav>
+
+        <Button variant="link" size="link">
+          Fazer login
+          <LogIn className="text-green-100 size-5" />
+        </Button>
+      </section>
+
+      <div className="absolute -top-24 rounded-full -left-[140px] size-56 bg-green-300 filter blur-3xl opacity-50 -z-10" />
+      <div className="absolute -top-24 rounded-full left-[140px] size-56 bg-purple-200 filter blur-3xl opacity-50 -z-10" />
+      <div className="absolute top-[50%] rounded-full left-[140px] size-56 bg-purple-200 filter blur-3xl opacity-50 -z-10" />
+      <div className="absolute -bottom-24 rounded-full -left-[140px] size-56 bg-green-200 filter blur-3xl opacity-50 -z-10" />
+    </aside>
+  )
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import { Sidebar } from '../components/sidebar'
+
+export default function Home() {
+  return (
+    <main className="bg-gray-800 text-gray-100 h-svh flex items-center justify-center gap-24">
+      <Sidebar />
+    </main>
+  )
+}

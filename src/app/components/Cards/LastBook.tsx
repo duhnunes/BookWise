164b2994b3a05f7a9947d5933c9fbdc7f1565d66
duhnunes <duhnunes.dev@gmail.com>
2024-09-25 15:06:20 -0300

    Add: LastBook component

---
+++ src/app/components/Cards/LastBook.tsx
@@ -0,0 +1,46 @@
+import { Star } from 'lucide-react'
+import Image from 'next/image'
+
+import bookImg from '../../../../public/images/books/o-hobbit.png'
+
+export const LastBook = () => {
+  return (
+    <section className="flex flex-col gap-y-8 px-6 py-5 bg-gray-600 rounded-md mb-5">
+      <article className="flex items-stretch gap-5">
+        <Image
+          src={bookImg}
+          width={108}
+          height={152}
+          alt=""
+          className="rounded max-w-[108px] max-h-[152px]"
+        />
+        <div className="flex flex-col justify-between">
+          <header className="flex flex-col">
+            <div className="flex justify-between">
+              <div className="flex-1 flex flex-col -space-y-1">
+                <span className="text-gray-400 text-sm">Há 2 dias</span>
+              </div>
+
+              <section className="inline-flex items-center">
+                <Star className="size-4 text-purple-100 fill-current" />
+                <Star className="size-4 text-purple-100 fill-current" />
+                <Star className="size-4 text-purple-100 fill-current" />
+                <Star className="size-4 text-purple-100 fill-current" />
+                <Star className="size-4 text-purple-100" />
+              </section>
+            </div>
+            <strong className="font-bold text-md leading-short text-gray-100">
+              O Hobbit
+            </strong>
+            <span className="text-gray-400 text-sm">J.R.R. Tolkien</span>
+          </header>
+
+          <p className="text-gray-300 w-full max-h-12 overflow-hidden">
+            Semper et sapien proin vitae nisi. Feugiat neque integer donec et
+            aenean posuere amet ultrices. Cras...
+          </p>
+        </div>
+      </article>
+    </section>
+  )
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,11 +3,13 @@ import { Sidebar } from '../components/sidebar'
 import { BookReview } from '../components/Cards/BookReview'
 import Link from 'next/link'
 import { BookCard } from '../components/Cards/BookCard'
+import { LastBook } from '../components/Cards/LastBook'
 
 export default function Home() {
   return (
     <main className="min-h-svh">
       <Sidebar />
+
       <section className="pr-24 flex flex-col pt-[72px] gap-10 ml-72">
         <h1 className="inline-flex items-center gap-3 text-gray-100 font-bold text-2xl">
           <ChartLine className="size-5 text-green-100" />
@@ -15,16 +17,31 @@ export default function Home() {
         </h1>
 
         <section className="flex items-start gap-x-16">
-          <div className="w-[608px] space-y-4">
-            <span className="text-gray-100 text-sm">
-              Avaliações mais recentes
-            </span>
-
-            <article className="flex flex-col gap-3">
-              <BookReview />
-              <BookReview />
-              <BookReview />
+          <div className="flex flex-col">
+            <article className="flex flex-col gap-4">
+              <div className="flex items-center justify-between">
+                <span className="text-gray-100 text-sm">
+                  Sua última leitura
+                </span>
+                <Link href="#" className="flex items-center gap-2">
+                  Ver todos
+                  <ChevronRight className="size-4" />
+                </Link>
+              </div>
+              <LastBook />
             </article>
+
+            <div className="w-[608px] space-y-4">
+              <span className="text-gray-100 text-sm">
+                Avaliações mais recentes
+              </span>
+
+              <article className="flex flex-col gap-3">
+                <BookReview />
+                <BookReview />
+                <BookReview />
+              </article>
+            </div>
           </div>
 
           <div className="w-[324px] space-y-4">

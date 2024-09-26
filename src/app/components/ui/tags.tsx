18dd105225667d73c8b60fb2c78fa2bc6e55a7ad
duhnunes <duhnunes.dev@gmail.com>
2024-09-26 12:30:42 -0300

    Change: Include 'data-state' to change layout

--- src/app/components/ui/tags.tsx
+++ src/app/components/ui/tags.tsx
@@ -1,14 +1,17 @@
+'use client'
+
 import * as React from 'react'
 
-interface TagsProps {
-  children: React.ReactNode
+interface TagsProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  dataState: 'on' | 'off'
 }
 
-export const Tags = ({ children }: TagsProps) => {
+export const Tags = ({ children, dataState }: TagsProps) => {
   return (
     <button
       type="button"
-      className="inline-flex items-center justify-center py-1 px-4 rounded-full whitespace-nowrap border border-purple-100 text-purple-100 text-md cursor-pointer hover:bg-purple-200 hover:text-white focus:bg-purple-200 focus:text-white focus:border-purple-200 transition-all"
+      className="inline-flex items-center justify-center py-1 px-4 rounded-full whitespace-nowrap border border-purple-100 text-purple-100 text-md cursor-pointer hover:bg-purple-200 hover:text-white data-[state=on]:bg-purple-200 data-[state=on]:text-white data-[state=on]:border-purple-200 transition-all"
+      data-state={dataState}
     >
       {children}
     </button>

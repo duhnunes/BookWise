2f1e64f1ec6b131e79cfa6a1c6ce6b7b11c6ae5a
duhnunes <duhnunes.dev@gmail.com>
2024-10-01 13:47:31 -0300

    Refactor: Login Button and Fix: When logged, all sidebar disappear

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { BinocularsIcon, ChartLine, LogIn, LogOut, User2 } from 'lucide-react'
 import Image from 'next/image'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import logo from '../../../public/logo.svg'
 import { Github } from './assets/github'
@@ -19,18 +19,6 @@ import {
 
 export const Sidebar = () => {
   const { data: session } = useSession()
-
-  if (session) {
-    return (
-      <>
-        <Button variant="link" size="link" className="mx-auto">
-          <Avatar />
-          {session?.user?.name}
-          <LogOut className="text-danger size-5" />
-        </Button>
-      </>
-    )
-  }
 
   return (
     <aside className="fixed bottom-5 top-5 left-5 rounded-xl bg-gray-700 pt-10 pb-6 overflow-hidden w-[14.5rem]">
@@ -66,35 +54,48 @@ export const Sidebar = () => {
           </ul>
         </nav>
 
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="link" size="link" className="mx-auto">
-              Fazer login
-              <LogIn className="text-green-100 size-5" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle className="text-center text-gray-200 font-bold leading-short text-md">
-                Faça login para deixar sua avaliação
-              </DialogTitle>
-            </DialogHeader>
-            <div className="flex flex-col gap-4">
-              <Button onClick={() => signIn('google')}>
-                <GoogleLogo />
-                <span className="text-gray-200 text-lg font-bold">
-                  Entrar com Google
-                </span>
+        {!session ? (
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="link" size="link" className="mx-auto">
+                Fazer login
+                <LogIn className="text-green-100 size-5" />
               </Button>
-              <Button onClick={() => signIn('github')}>
-                <Github />
-                <span className="text-gray-200 text-lg font-bold">
-                  Entrar com Github
-                </span>
-              </Button>
-            </div>
-          </DialogContent>
-        </Dialog>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="text-center text-gray-200 font-bold leading-short text-md">
+                  Faça login para deixar sua avaliação
+                </DialogTitle>
+              </DialogHeader>
+              <div className="flex flex-col gap-4">
+                <Button onClick={() => signIn('google')}>
+                  <GoogleLogo />
+                  <span className="text-gray-200 text-lg font-bold">
+                    Entrar com Google
+                  </span>
+                </Button>
+                <Button onClick={() => signIn('github')}>
+                  <Github />
+                  <span className="text-gray-200 text-lg font-bold">
+                    Entrar com Github
+                  </span>
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
+        ) : (
+          <Button
+            variant="link"
+            size="link"
+            className="mx-auto"
+            onClick={() => signOut()}
+          >
+            <Avatar />
+            {session?.user?.name}
+            <LogOut className="text-danger size-5" />
+          </Button>
+        )}
       </section>
 
       <div className="absolute -top-24 rounded-full -left-[140px] size-56 bg-green-300 filter blur-3xl opacity-50 -z-10" />
